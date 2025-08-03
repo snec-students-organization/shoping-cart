94ba03855c74ac2c171668f6056a183b9cc6ce3d
ramees9207 <rameespc17@gmail.com>
2025-08-03 07:07:16 +0530

    final

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,15 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGO_URI || 'mongodb+srv://rameespc17:Ramees1234@cluster0.h9gnra4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).catch(err => {
-  console.error('Initial connection error:', err);
-});
+mongoose.connect(mongoURI);
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
-});
-
-db.on('disconnected', () => {
-  console.warn('MongoDB connection lost. Attempting reconnect...');
 });
 
 module.exports = db;
